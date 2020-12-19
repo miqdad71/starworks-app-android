88863f7dbafb925bdf4aac34cce47238a9de89b3
miqdad71 <miqdad.ihbs71777@gmail.com>
2020-12-20 00:24:32 +0700

    navbar

--- app/src/main/java/com/miqdad71/starworks/view/activities/CoreActivity.kt
+++ app/src/main/java/com/miqdad71/starworks/view/activities/CoreActivity.kt
@@ -3,10 +3,13 @@ package com.miqdad71.starworks.view.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.miqdad71.starworks.R
+import kotlinx.android.synthetic.main.activity_core.*
 
 class CoreActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_core)
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
     }
 }
