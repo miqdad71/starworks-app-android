3e281bbe307260cd465f3e9ccd03b5248118d4cd
miqdad71 <miqdad.ihbs71777@gmail.com>
2020-12-23 11:01:44 +0700

    Mencoba DialogActivity.kt

--- app/src/main/java/com/miqdad71/starworks/view/dialog/DialogActivity.kt
+++ app/src/main/java/com/miqdad71/starworks/view/dialog/DialogActivity.kt
@@ -18,12 +18,33 @@ class DialogActivity : AppCompatActivity() {
         binding.btn1.setOnClickListener {
             showDialogOne()
         }
+        binding.btn2.setOnClickListener {
+            showDialogTwo()
+        }
+        binding.btn3.setOnClickListener {
+            showDialogThree()
+        }
+
     }
 
     private fun  showDialogOne(){
         val dialog = AlertDialog.Builder(this)
             .setTitle("Test Dialog One")
             .setMessage("Ini Dialog 1")
+            .create()
+        dialog.show()
+    }
+    private fun  showDialogTwo(){
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Test Dialog Two")
+            .setMessage("Ini Dialog 2")
+            .create()
+        dialog.show()
+    }
+    private fun  showDialogThree(){
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Test Dialog Three")
+            .setMessage("Ini Dialog 3")
             .create()
         dialog.show()
     }
