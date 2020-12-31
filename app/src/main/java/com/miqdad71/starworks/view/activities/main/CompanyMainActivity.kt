928f22b0f1927dbce7d958cb664002e406b6bec8
miqdad71 <miqdad.ihbs71777@gmail.com>
2020-12-31 13:16:19 +0700

    layouting

--- app/src/main/java/com/miqdad71/starworks/view/activities/main/CompanyMainActivity.kt
+++ app/src/main/java/com/miqdad71/starworks/view/activities/main/CompanyMainActivity.kt
@@ -1,26 +1,23 @@
 package com.miqdad71.starworks.view.activities.main
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.miqdad71.starworks.R
 import com.miqdad71.starworks.databinding.ActivityEngineerMainBinding
 import com.miqdad71.starworks.view.fragments.engineer.EngineerHomeFragment
 import com.miqdad71.starworks.view.fragments.engineer.EngineerJobsFragment
-import com.miqdad71.starworks.view.fragments.engineer.EngineerSearchFragment
 import com.miqdad71.starworks.view.fragments.engineer.EngineerProfileEngineerFragment
+import com.miqdad71.starworks.view.fragments.engineer.EngineerSearchFragment
 import kotlinx.android.synthetic.main.activity_engineer_main.*
 
-class EngineerMainActivity : AppCompatActivity() {
-   private lateinit var binding: ActivityEngineerMainBinding
+class CompanyMainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEngineerMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-//      bottomNavigationView.background = null
-//      binding.bottomNavigationView.menu.getItem(2).isEnabled = false
-//      binding.bottomNavigationView.background = null
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_engineer_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_company_main)
 
         val firstFragment = EngineerHomeFragment()
         val secondFragment = EngineerSearchFragment()
@@ -38,11 +35,10 @@ class EngineerMainActivity : AppCompatActivity() {
             }
             true
         }
-    }
-
+}
     private fun setCurrentFragment(fragment: Fragment) =
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.flFragment, fragment)
-                commit()
-            }
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
 }
