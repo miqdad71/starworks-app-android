9f29847ffdd2bf0cdac0cf8e3df7a3733691617f
miqdad71 <miqdad.ihbs71777@gmail.com>
2021-01-05 16:36:01 +0700

    SignUpActivity.kt beginning

--- app/src/main/java/com/miqdad71/starworks/data/model/engineer/EngineerModel.kt
+++ app/src/main/java/com/miqdad71/starworks/data/model/engineer/EngineerModel.kt
@@ -1,9 +1,11 @@
 package com.miqdad71.starworks.data.model.engineer
 
 data class EngineerModel(
-    var name: String? = null,
-    var email: String? = null,
-    var phone: Long = 0,
-    var password: String? = null,
+    var acName: String? = null,
+    var acEmail: String? = null,
+    var acPassword: String? = null,
+    var acPhone: Long = 0,
+    var acPosition: String? = null,
+    var acCompany: String? = null,
     var isLogin: Boolean = false
 )
--- app/src/main/java/com/miqdad71/starworks/ui/activities/signup/SignUpActivity.kt
+++ app/src/main/java/com/miqdad71/starworks/ui/activities/signup/SignUpActivity.kt
@@ -3,19 +3,28 @@ package com.miqdad71.starworks.ui.activities.signup
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.miqdad71.starworks.R
+import com.miqdad71.starworks.api.AccountAPI
+import com.miqdad71.starworks.data.model.account.LoginResponse
 import com.miqdad71.starworks.databinding.ActivitySignUpBinding
 import com.miqdad71.starworks.util.SharedPreference
 import com.miqdad71.starworks.ui.activities.main.engineer.EngineerMainActivity
 import com.miqdad71.starworks.ui.activities.login.LoginActivity
 import com.miqdad71.starworks.ui.activities.dialog.Dialog
 import com.miqdad71.starworks.data.model.engineer.EngineerModel
+import com.miqdad71.starworks.data.remote.ApiClient
+import com.miqdad71.starworks.ui.activities.forgetpassword.ForgetPasswordVerifyActivity
+import com.miqdad71.starworks.ui.activities.main.company.CompanyMainActivity
+import kotlinx.coroutines.*
+import retrofit2.HttpException
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity(), View.OnClickListener {
 
     companion object {
         const val FIELD_REQUIRED = "Fields cannot be empty"
@@ -24,13 +33,16 @@ class SignUpActivity : AppCompatActivity() {
         const val FIELD_MUST_MATCH = "Password must be the same"
     }
 
-    private var engineerModel: EngineerModel = EngineerModel()
     private lateinit var binding: ActivitySignUpBinding
     private lateinit var dialog: Dialog
+    private lateinit var preference: SharedPreference
+    private lateinit var coroutineScope: CoroutineScope
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+        coroutineScope = CoroutineScope(Job() + Dispatchers.Main)
+
         supportActionBar?.hide()
         window.setFlags(
             WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
@@ -43,86 +55,87 @@ class SignUpActivity : AppCompatActivity() {
             binding.clCompany.visibility = View.GONE
         }
 
-        // sign-up action
-        binding.btnSignUp.setOnClickListener {
-            registration()
-        }
+        binding.btnSignUp.setOnClickListener(this)
+        binding.tvLogin.setOnClickListener(this)
 
-        binding.tvLogin.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_sign_up -> {
+                signUp(v)
+            }
+            R.id.tv_login -> {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
         }
     }
 
-    private fun registration() {
-        dialog = Dialog()
-        val name = binding.etName.text.toString()
+    private fun signUp(view: View) {
+        val nama = binding.etName.text.toString()
         val email = binding.etEmail.text.toString()
+        val phone = binding.etPhoneNumber.text.toString()
         val password = binding.etPassword.text.toString()
-        val passwordconfirm = binding.etPasswordConfirm.text.toString()
-        val phonenumber = binding.etPhoneNumber.text.toString()
-        val position = binding.etPosition.text.toString()
-        val company = binding.etCompany.text.toString()
+        val passwordconfirmation = binding.etPasswordConfirm.text.toString()
 
-        if (name.isEmpty()) {
+        preference = SharedPreference(view.context)
+
+        if (password.isEmpty()) {
             binding.etName.error = FIELD_REQUIRED
             return
         }
-
         if (email.isEmpty()) {
-            binding.etEmail.error = FIELD_IS_NOT_VALID
+            binding.etEmail.error = FIELD_REQUIRED
             return
         }
-
+        if (password.isEmpty()) {
+            binding.etPhoneNumber.error = FIELD_REQUIRED
+            return
+        }
         if (password.isEmpty()) {
             binding.etPassword.error = FIELD_REQUIRED
             return
         }
-
-        if (passwordconfirm.isEmpty()) {
+        if (password.isEmpty()) {
             binding.etPasswordConfirm.error = FIELD_REQUIRED
             return
         }
+        signUpAccount()
+    }
+    fun signUpAccount() {
+        val api = ApiClient.getApiClient(this).create(AccountAPI::class.java)
+        coroutineScope.launch {
+            val res = withContext(Dispatchers.IO) {
+                try {
+                    api.login(
+                        binding.etEmail.text.toString(),
+                        binding.etPassword.text.toString()
+                    )
 
-        if (password != passwordconfirm) {
-            binding.etPasswordConfirm.error = FIELD_MUST_MATCH
-            return
-        }
+                } catch (t: Exception) {
+                    Log.e("Error", t.localizedMessage)
+                }
+            }
 
-        if (phonenumber.isEmpty()) {
-            binding.etPhoneNumber.error = FIELD_REQUIRED
-            return
-        }
-
-        if (!TextUtils.isDigitsOnly(phonenumber)) {
-            binding.etPhoneNumber.error = FIELD_DIGITS_ONLY
-            return
-        }
-        saveData(name, email, password, phonenumber, true)
-
-        dialog.dialog(this, "Register Successful") {
-            val sendIntent = Intent(this, LoginActivity::class.java)
-            startActivity(sendIntent)
+            if (res is LoginResponse) {
+                val data = res.data
+                Log.d("loginData", data.toString())
+                val level = data.acLevel
+                preference.setAccount(data.acName, data.acId, data.acLevel, data.acEmail)
+                preference.setToken(data.token)
+                when(level) {
+                    0 -> {
+                        val sendIntent = Intent(this@SignUpActivity, EngineerMainActivity::class.java)
+                        startActivity(sendIntent)
+                        this@SignUpActivity.finish()
+                    }
+                    1 -> {
+                        val sendIntent = Intent(this@SignUpActivity, CompanyMainActivity::class.java)
+                        startActivity(sendIntent)
+                        this@SignUpActivity.finish()
+                    }
+                }
+            }
         }
     }
-
-    private fun saveData(
-        name: String,
-        email: String,
-        password: String,
-        phone: String,
-        isLogin: Boolean
-    ) {
-        val userPreference = SharedPreference(this)
-
-        engineerModel.name = name
-        engineerModel.email = email
-        engineerModel.password = password
-        engineerModel.phone = phone.toLong()
-        engineerModel.isLogin = isLogin
-//        engineerModel.company = company
-//        engineerModel.position = position
-
-        userPreference.setEngineerPreference(engineerModel)
-    }
-
 }
